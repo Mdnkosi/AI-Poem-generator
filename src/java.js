0f08cac76038d9e4b22f30c0cfb88748c420ccf3
Mdnkosi <mahlatsedineo93@gmail.com>
2025-03-08 18:03:21 +0200

    Added JavaScript functionality and user experience.

--- src/java.js
+++ src/java.js
@@ -14,13 +14,17 @@ function generatePoem(event) {
 
   let userInput = document.querySelector("#user-instruction")
   let apiKey = "t0dbadd020obaf40a394084940d3fadf";
-  let context = "You are great english native poet.Your mission is to generate a short poem and follow the prompt. Keep the poem to 5 lines and dispaly in sonnet structure,make the structure of the poem a sonnet structure.Seperate each line with the HTML <br> and add a  generated heading  at the top with a HTML <strong> element with a font-size of 1.5 to the body's size.Make it centre aligned "
+  let context = "You are great english native poet.Your mission is to generate a short poem and follow the prompt. Keep the poem to 5 lines and dispaly in sonnet structure,make the structure of the poem a sonnet structure.Seperate each line with the HTML <br> and add a  generated heading  at the top with a HTML <strong> element with a font-size of 1.5 to the body's size.Make the whole poem centre aligned "
   let prompt = `generate a poem about ${userInput.value}`
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
-  console.log(`prompt:${ prompt }`);
-  console.log(`context${context}`)
+
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⌛Generating a poem about ${userInput.value}...</div>`;
+
+  
 axios.get(apiUrl).then(displayPoem)
 
 }
